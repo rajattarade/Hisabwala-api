98234534fd363051bbda57dd2a4f8d8bb6754dd2
Rajat Tarade C137 <rtarade@gmail.com>
2025-10-21 14:21:35 +0530

    Minor Improments (#4)

--- src/Hisabwala.Application/Features/Party/GetPartyInformation/GetPartyInformationCommand.cs
+++ src/Hisabwala.Application/Features/Party/GetPartyInformation/GetPartyInformationCommand.cs
@@ -5,6 +5,6 @@ namespace Hisabwala.Application.Features.Party.GetPartyInformation
 {
     public class GetPartyInformationCommand : IRequest<Result<PartyInformationDTO>>
     {
-        public string PartyCode { get; set; } = null!;
+        public string? PartyCode { get; set; }
     }
 }
